1a9e830a702b6ebca2f92bdb77313468964863f1
Fin <finleywarman@hotmail.co.uk>
2021-12-07 13:25:10 +0000

    Use day 7 01 to use median solution

--- 07/01.py
+++ 07/01.py
@@ -1,21 +1,28 @@
 #! /usr/bin/env python3
 import re
 import math
+import statistics
 
 # ==== INPUT ====
 data = ""
 f = '07.txt'
-#f = 'demo.txt'
+# f = 'demo.txt'
 with open(f, 'r') as file:
     data = file.read()
 
 nums = sorted([int(x) for x in data.split(',')])
 # ==== SOLUTION ====
 
-minf = nums[-1] * len(nums)
-for num in nums:
-    fuel = 0
-    for x in nums:
-        fuel += abs(x - num)
-    minf = min(fuel, minf)
-print(minf)
+# median solution
+mid = nums[len(nums)//2]
+fuel = sum([abs(x - mid) for x in nums])
+print(fuel)
+
+# bruteforce solution
+# minf = nums[-1] * len(nums)
+# for num in nums:
+#     fuel = 0
+#     for x in nums:
+#         fuel += abs(x - num)
+#     minf = min(fuel, minf)
+# print(minf)
