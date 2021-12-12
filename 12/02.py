3f767a4dd1c694a8bbcfb0222615d5bc3e68023b
Fin <finleywarman@hotmail.co.uk>
2021-12-12 14:56:16 +0000

    AoC Day 12 part 2

---
+++ 12/02.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python3
+from os import link, path
+import re
+import math
+from collections import defaultdict
+
+# ==== INPUT ====
+data = ""
+f = '12.txt'
+#f = 'demo.txt'
+with open(f, 'r') as file:
+    data = file.read()
+
+rows = [row.strip() for row in data.split('\n')[:-1]]
+
+# ==== SOLUTION ====
+
+links = defaultdict(set)
+
+uppers = set()
+lowers = set()
+
+for row in rows:
+    lhs, rhs = row.split("-")
+    links[lhs].add(rhs)
+    links[rhs].add(lhs)
+    for x in lhs, rhs:
+        if x.lower() == x:
+            lowers.add(x)
+        else:
+            uppers.add(x)
+
+links = dict(links)
+paths = set()
+
+
+def dfs(curr_node, visited, path, small_allowed):
+    global paths
+    visited[curr_node] += 1
+
+    if curr_node == 'end':
+        paths.add(','.join(path))
+    else:
+        if (curr_node in lowers and visited[curr_node] > 1):
+            if curr_node == 'start':
+                return
+            elif small_allowed:
+                small_allowed = False
+                pass
+            else:
+                return
+
+        for node in links[curr_node]:
+            dfs(node, visited.copy(), path + [curr_node], small_allowed)
+
+
+dfs('start', defaultdict(int), [], True)
+
+# for path in paths:
+#     print(path)
+
+print(len(paths))
